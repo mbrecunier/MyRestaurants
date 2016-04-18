dd67e9c143d08438e3c98f1bd303bda329408df4
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-18 10:40:07 -0700

    gather user inputted location

--- app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String location = mLocationEditText.getText().toString();
                 Log.d(TAG, location);
-               Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
+                Intent intent = new Intent(MainActivity.this, RestaurantsActivity.class);
                 startActivity(intent);
             }
         });
