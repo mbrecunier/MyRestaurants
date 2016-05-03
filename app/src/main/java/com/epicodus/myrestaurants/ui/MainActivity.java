b01e68e79c46f66af78cd92fce3d1ca94f465683
Anna Becker and Michelle Brecunier <anna.r.becker@gmail.com>
2016-05-03 08:19:44 -0700

    remove edittext from main activity and add new search widget

--- app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/MainActivity.java
@@ -6,6 +6,9 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -22,59 +25,35 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     public static final String TAG = MainActivity.class.getSimpleName();
-    private Firebase mSearchedLocationRef;
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
-    private ValueEventListener mSearchedLocationRefListener;
-
-
     @Bind(R.id.findRestaurantsButton) Button mFindRestaurantsButton;
     @Bind(R.id.savedRestaurantsButton) Button mSavedRestaurantsButton;
-    @Bind(R.id.locationEditText) EditText mLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
-        mSearchedLocationRef = new Firebase(Constants.FIREBASE_URL_SEARCHED_LOCATION);
         mFindRestaurantsButton.setOnClickListener(this);
         mSavedRestaurantsButton.setOnClickListener(this);
-
-        mSearchedLocationRefListener = mSearchedLocationRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String locations = dataSnapshot.getValue().toString();
-                Log.d("Location updated", locations);
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        });
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mSearchedLocationRef.removeEventListener(mSearchedLocationRefListener);
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.findRestaurantsButton:
-                String location = mLocationEditText.getText().toString();
-                saveLocationToFirebase(location);
-//                if(!(location).equals("")) {
-//                    addToSharedPreferences(location);
-//                }
                 Intent intent = new Intent(MainActivity.this, RestaurantListActivity.class);
-                intent.putExtra("location", location);
                 startActivity(intent);
                 break;
             case R.id.savedRestaurantsButton:
