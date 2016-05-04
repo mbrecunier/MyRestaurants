3ab4708a58d3b94c233d9b43a64fddf1398e3e67
Michelle Brecunier and Tyler Suderman <michelle.brecunier@gmail.com>
2016-05-04 10:27:55 -0700

    create a new user in Firebase when user submits CreateAccountActivity information

--- app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/myrestaurants/ui/CreateAccountActivity.java
@@ -2,14 +2,66 @@ package com.epicodus.myrestaurants.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.epicodus.myrestaurants.Constants;
 import com.epicodus.myrestaurants.R;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import java.util.Map;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = CreateAccountActivity.class.getSimpleName();
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView)
+    TextView mLoginTextView;
+    private Firebase mFirebaseRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        ButterKnife.bind(this);
+        mFirebaseRef = new Firebase(Constants.FIREBASE_URL);
+        mCreateUserButton.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        if (view == mCreateUserButton) {
+            createNewUser();
+        }
+    }
+
+    public void createNewUser() {
+        final String name = mNameEditText.getText().toString();
+        final String email = mEmailEditText.getText().toString();
+        final String password = mPasswordEditText.getText().toString();
+        final String confirmPassword = mConfirmPasswordEditText.getText().toString();
+        mFirebaseRef.createUser(email, password, new Firebase.ValueResultHandler<Map<String, Object>>() {
+            @Override
+            public void onSuccess(Map<String, Object> result) {
+            }
+
+            @Override
+            public void onError(FirebaseError firebaseError) {
+                Log.d(TAG, "error occurred " +
+                        firebaseError);
+            }
+        });
+    }
+
+
 }
