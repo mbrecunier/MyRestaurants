3277d25b708a789330833158f61a107f480d2509
Michelle Brecunier and Tyler Suderman <Guest@epicodus-96.local>
2016-05-04 09:42:24 -0700

    create Login and CreateAccount activities with Login launching at start of app

--- app/src/main/java/com/epicodus/myrestaurants/Constants.java
+++ app/src/main/java/com/epicodus/myrestaurants/Constants.java
@@ -15,4 +15,9 @@ public class Constants {
     public static final String FIREBASE_URL_SEARCHED_LOCATION = FIREBASE_URL + "/" + FIREBASE_LOCATION_SEARCHED_LOCATION;
     public static final String FIREBASE_LOCATION_RESTAURANTS = "restaurants";
     public static final String FIREBASE_URL_RESTAURANTS = FIREBASE_URL + "/" + FIREBASE_LOCATION_RESTAURANTS;
+
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_PROPERTY_EMAIL = "email";
+    public static final String KEY_UID = "UID";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
 }
